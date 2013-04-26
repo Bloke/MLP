Bloke/MLP

commit 4910293723b84994661cadd068e325d13d23296f

Use cookies instead of sessions
While _SESSION is convenient, it's technically unnecessary. The fact
that l10n is loaded first means other plugins that come along later and
do legitimately require sessions with classes may not work properly
because the __PHP_Incomplete_Class error appears if the session has
begun before the class definition. This affects things such as shopping
cart plugins (e.g. yab_shop) and cannot easily be resolved -- even with
an autoloader -- because MLP is a hack, not a plugin.

Hopefully this patch is a drop-in replacement for session variables,
with the possible exception that manually altering the URL to remove the
lang marker will always return the site to its default language instead
of retaining the previously used language and just not showing it in the
URL.  Some might argue this is an improvement.

plugins/sources/l10n.php
@@ -105,7 +105,6 @@ function _l10n_process_url( $use_get_params=false )
 	$new_first_path = '';
 	$debug = (0) && (@txpinterface === 'public');
 
-	@session_start();
 	$site_langs = MLPLanguageHandler::get_site_langs();
 
 	$req_method = serverSet('REQUEST_METHOD');
@@ -156,7 +155,7 @@ function _l10n_process_url( $use_get_params=false )
 	if( $use_get_params )
 		{
 		#
-		#	Admin session variables differ from public to stop crosstalk...
+		#	Admin lang cookies differ from public to stop crosstalk...
 		#
 		$ssname = 'l10n_admin_short_lang';
 		$lsname = 'l10n_admin_long_lang';
@@ -174,9 +173,9 @@ function _l10n_process_url( $use_get_params=false )
 			#
 			#	Hit! We can serve this language...
 			#
-			$_SESSION[$ssname] = $tmp;
-			$_SESSION[$lsname] = $temp;
-			if( $debug ) echo br , "L10N MLP: Set session vars ($ssname < $tmp) ($lsname < $temp).";
+			_l10n_set_cookie($ssname, $tmp);
+			_l10n_set_cookie($lsname, $temp);
+			if( $debug ) echo br , "L10N MLP: Set lang cookie ($ssname < $tmp) ($lsname < $temp).";
 			}
 		}
 
@@ -197,8 +196,8 @@ function _l10n_process_url( $use_get_params=false )
 		if( is_array($callback_detect_language) and isset($callback_detect_language['lang']) and isset($callback_detect_language['lang']['long']) and in_array( $callback_detect_language['lang']['long'] , $site_langs ) )
 			{
 			if( $debug ) echo br , "L10N MLP: Plugin callback returned " , var_dump($callback_detect_language);
-			$_SESSION[$ssname] = $callback_detect_language['lang']['short'];
-			$_SESSION[$lsname] = $callback_detect_language['lang']['long'];
+			_l10n_set_cookie($ssname, $callback_detect_language['lang']['short']);
+			_l10n_set_cookie($lsname, $callback_detect_language['lang']['long']);
 			$reduced_uri = $callback_detect_language['uri']['reduced'];
 			$new_first_path = array_shift(explode('/', ltrim($reduced_uri, '/'), 2));
 			$u1 = $callback_detect_language['uri']['code'];
@@ -218,9 +217,9 @@ function _l10n_process_url( $use_get_params=false )
 				#
 				#	Hit! We can serve this language...
 				#
-				if( $debug ) echo br , "L10N MLP: Set session vars ($ssname < $u1) ($lsname < $temp).";
-				$_SESSION[$ssname] = $u1;
-				$_SESSION[$lsname] = $temp;
+				if( $debug ) echo br , "L10N MLP: Set lang cookies ($ssname < $u1) ($lsname < $temp).";
+				_l10n_set_cookie($ssname, $u1);
+				_l10n_set_cookie($lsname, $temp);
 				}
 			else
 				{
@@ -246,8 +245,8 @@ function _l10n_process_url( $use_get_params=false )
 			# commenting and plugins...
 			#
 			$lang_code_pos = strpos( $_SERVER['REQUEST_URI'] , "/$u1/" );
-			$_SESSION['l10n_request_uri'] = substr( $_SERVER['REQUEST_URI'] , 0 , $lang_code_pos+1 ).
-				substr( $_SERVER['REQUEST_URI'] , $lang_code_pos+strlen($u1)+2, strlen($_SERVER['REQUEST_URI']) );
+			_l10n_set_cookie('l10n_request_uri', substr( $_SERVER['REQUEST_URI'] , 0 , $lang_code_pos+1 ).
+				substr( $_SERVER['REQUEST_URI'] , $lang_code_pos+strlen($u1)+2, strlen($_SERVER['REQUEST_URI']) ) );
 
 			if( !( $new_uri = $reduced_uri ) )
 				$new_uri = substr( $req , strlen($u1)+1 );
@@ -261,7 +260,7 @@ function _l10n_process_url( $use_get_params=false )
 	if( @$prefs['l10n_l10n-use_browser_languages'] == '1' )
 		{
 		if( $debug ) echo br,br,"Checking browser accept-language headers.",br,br;
-		if( !isset($_SESSION[$ssname]) or empty($_SESSION[$ssname]) )
+		if( !isset($_COOKIE[$ssname]) or empty($_COOKIE[$ssname]) )
 			{
 			#
 			#	If we are still missing a language for the session, try to get the prefered selection
@@ -301,8 +300,8 @@ function _l10n_process_url( $use_get_params=false )
 
 							if( in_array( $lang['long'] , $site_langs ) )
 								{
-								$_SESSION[$ssname] = $lang['short'];
-								$_SESSION[$lsname] = $lang['long'];
+								_l10n_set_cookie($ssname, $lang['short']);
+								_l10n_set_cookie($lsname, $lang['long']);
 								if( $debug ) echo 'Setting language to '.$lang['long'].' from browser headers',br,br;
 								break;
 								}
@@ -316,24 +315,24 @@ function _l10n_process_url( $use_get_params=false )
 	#
 	#	If we are still missing a language for the session, use the site default...
 	#
-	if( !isset($_SESSION[$ssname]) or empty($_SESSION[$ssname]) )
+	if( !isset($_COOKIE[$ssname]) or empty($_COOKIE[$ssname]) )
 		{
 		$long = $site_langs[0];
 		$short = substr( $long , 0 , 2 );
-		$_SESSION[$ssname] = $short;
-		$_SESSION[$lsname] = $long;
+		_l10n_set_cookie($ssname, $short);
+		_l10n_set_cookie($lsname, $long);
 		if( $debug ) echo br , "L10N MLP: No language match found, setting to site default ... $long as $short";
 		}
 
 	$default_lang = MLPLanguageHandler::compact_code( MLPLanguageHandler::get_site_default_lang() );
-	if ( $redirect and ($_SESSION[$ssname] == $default_lang['short']) ) $redirect = false;
+	if ( $redirect and ($_COOKIE[$ssname] == $default_lang['short']) ) $redirect = false;
 
 	if( $redirect )
 		{
 		$callback_language_marker = false;
 		if( isset($prefs['l10n_language_marker_func']) and is_callable($prefs['l10n_language_marker_func']) )
-			$callback_language_marker = call_user_func( $prefs['l10n_language_marker_func'], $_SESSION[$lsname] );
-		if( !$callback_language_marker ) $callback_language_marker = $_SESSION[$ssname];
+			$callback_language_marker = call_user_func( $prefs['l10n_language_marker_func'], $_COOKIE[$lsname] );
+		if( !$callback_language_marker ) $callback_language_marker = $_COOKIE[$ssname];
 		$location = hu.$callback_language_marker.'/'; # QUESTION: Does this need a trailing slash?
 
 		if( $debug )
@@ -350,8 +349,8 @@ function _l10n_process_url( $use_get_params=false )
 			}
 		}
 
-	if( _l10n_set_browse_language( $_SESSION[$lsname] , true , $debug ) );
-	else _l10n_set_browse_language( $_SESSION[$ssname] , false , $debug );
+	if( _l10n_set_browse_language( $_COOKIE[$lsname] , true , $debug ) );
+	else _l10n_set_browse_language( $_COOKIE[$ssname] , false , $debug );
 
 	if( $debug ) echo br , "New first path is: $new_first_path";
 	return $new_first_path;
@@ -471,9 +470,9 @@ if (@txpinterface === 'public')
 		#	The REQUEST_URI has to be maintained to ensure comments work and compatibility with
 		# plugins...
 		#
-		if ($prefs['l10n_l10n-url_default_lang_marker'] === '1' || isset($_SESSION['l10n_request_uri']))
+		if ($prefs['l10n_l10n-url_default_lang_marker'] === '1' || isset($_COOKIE['l10n_request_uri']))
 		{
-			$pretext['request_uri'] = $_SERVER['REQUEST_URI'] = $_SESSION['l10n_request_uri'];
+			$pretext['request_uri'] = $_SERVER['REQUEST_URI'] = $_COOKIE['l10n_request_uri'];
 		}
 
 		#

textpattern/lib/l10n_base.php
@@ -439,6 +439,13 @@ function _l10n_make_textpattern_name( $full_code )
 	return $result;
 	}
 
+function _l10n_set_cookie( $name, $value )
+	{
+		setcookie($name, $value, 0, '/', false, true);
+
+		// Kludge to simulate session var :-(
+		if (!isset($_COOKIE[$name])) $_COOKIE[$name] = $value;
+	}
 
 
 class MLPLanguageHandler

textpattern/lib/l10n_admin_classes.php
@@ -5042,15 +5042,14 @@ class MLPWizView extends GBPWizardTabView
 		$this->add_report_item( gTxt( 'l10n-setup_2_langs' , array( '{langs}' => join( ', ' , $languages ) ) ) , true );
 
 		#
-		#	Reset the session variable (in case of a language switch and then a reinstall)...
+		#	Reset the lang cookie (in case of a language switch and then a reinstall)...
 		#
-		@session_start();
 		$temp = LANG;
 		$tmp = substr( $temp , 0 , 2 );
 		if( !empty($temp) )
 			{
-			$_SESSION['l10n_admin_short_lang'] = $tmp;
-			$_SESSION['l10n_admin_long_lang']  = $temp;
+			_l10n_set_cookie('l10n_admin_short_lang', $tmp);
+			_l10n_set_cookie('l10n_admin_long_lang', $temp);
 			}
 
 

textpattern/lib/txplib_db.php
@@ -621,19 +621,18 @@ $DB = new DB;
 				if( l10n_installed( true ) )
 					{
 					#	First call and the plugin is installed and active so guess
-					# which language the user is browsing in based on the long session variable.
+					# which language the user is browsing in based on the long lang variable.
 					# This will not be set for the first visit to a page but it does reduce the need
 					# for reloading the strings.
 					#
 					# If this guess later proves to be wrong -- for example, on the first call or when
 					# the user switches browse language -- then $textarray will be reloaded.
 					#
-					session_start();
 					$language = '';
 					if( @txpinterface==='admin' )
-						$language = @$_SESSION['l10n_admin_long_lang'];
+						$language = @$_COOKIE['l10n_admin_long_lang'];
 					else
-						$language = @$_SESSION['l10n_long_lang'];
+						$language = @$_COOKIE['l10n_long_lang'];
 
 					if( !empty( $language ) )
 						{

commit 5ae1b388f3c9aaa26025e02e254295f9275fdf3d

Helps if cookies aren't secure only
Commit 4910293723b84994661cadd068e325d13d23296f introduced a regression
for searches whereby the default site language was returned when using
?q= with the txp:search_input tag. Now, when a lang marker is omitted,
the site reverts to the previous language, as stored in the cookie,
which retains the old session-based functionality.

plugins/sources/l10n.php
@@ -179,7 +179,7 @@ function _l10n_process_url( $use_get_params=false )
 			}
 		}
 
-	if( !$use_get_params and !empty( $u1 ) )
+	if( !$use_get_params )
 		{
 		if( $debug ) echo br , "L10N MLP: Public - Checking URL ($req), LANG = " , LANG;
 
@@ -204,7 +204,11 @@ function _l10n_process_url( $use_get_params=false )
 			}
 		else
 			{
-			if (empty($temp))
+			if (empty( $u1 ))
+				{
+					$u1 = isset($_COOKIE[$ssname]) ? $_COOKIE[$ssname] : '';
+				}
+			if (empty($temp) )
 				{
 				if( $debug ) echo br , 'L10N MLP: Checking start of path for language ... ' . $u1;
 				$temp = MLPLanguageHandler::expand_code( $u1 );
@@ -522,17 +526,17 @@ if (@txpinterface === 'public')
 			#	Prevent the feed routine(s) from removing our handler!
 			if (extension_loaded('zlib') && ini_get('zlib.output_compression') == 0 && ini_get('output_handler') != 'ob_gzhandler' && !headers_sent())
 				{
-				@ob_start('ob_gzhandler');
+				ob_start('ob_gzhandler');
 				if( $prefs['l10n_l10n-clean_feeds'] == '0' )
 					ini_set( 'zlib.output_compression' , 1 );
 				}
 
 			#	Inject our language markers into the feed stream...
-			@ob_start( '_l10n_inject_'.$first_chunk.'_lang_markers' );
+			ob_start( '_l10n_inject_'.$first_chunk.'_lang_markers' );
 			}
 		elseif( $first_chunk !== 'file_download' )
 			{
-			@ob_start('_l10n_inject_lang_markers');
+			ob_start('_l10n_inject_lang_markers');
 			}
 		}
 	function _l10n_markup( $s , $quote = false )

textpattern/lib/l10n_base.php
@@ -441,10 +441,10 @@ function _l10n_make_textpattern_name( $full_code )
 
 function _l10n_set_cookie( $name, $value )
 	{
-		setcookie($name, $value, 0, '/', false, true);
+		setcookie($name, $value, 0, '/', false, false);
 
 		// Kludge to simulate session var :-(
-		if (!isset($_COOKIE[$name])) $_COOKIE[$name] = $value;
+		$_COOKIE[$name] = $value;
 	}
 
 